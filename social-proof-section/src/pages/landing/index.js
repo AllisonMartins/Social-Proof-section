f007c5175df38151a944749f6d559584893c7e1d
Allison Martins <allison.martins99@gmail.com>
2020-09-15 14:41:17 -0300

    mobile first complete

--- social-proof-section/src/pages/landing/index.js
+++ social-proof-section/src/pages/landing/index.js
@@ -1,31 +1,101 @@
 import React from 'react';
 import backgroundMobile from '../../assets/images/bg-pattern-top-mobile.svg';
-import style from './styles.css'
+import starIcon from '../../assets/images/icon-star.svg';
+import style from './styles.css';
+import avatar1 from '../../assets/images/image-colton.jpg';
+import avatar2 from '../../assets/images/image-anne.jpg';
+import avatar3 from '../../assets/images/image-irene.jpg';
+
+
+
 
 function Landing(){
     return(
-        <div id="page-landing">
+        <main>
             <div className="container" id="page-landing-content">
         <div className="header-landing" styles={{ backgroundImage:`url(${backgroundMobile})` }}>
-                    <p>10,000+ of our</p>
-                    <p>users love our</p>
-                    <p>products.</p>
+                    <p>10,000+ of our
+                    users love our
+                    products.</p>
                 </div>
                 <div className="subject-header">
                 
-                <p>We only provide great products combined </p><p> with excellent customer service. See what</p>
-                <p>our satisfied customers are saying about</p><p> our services.
+                <p>We only provide great products combined with excellent customer service. See what
+                our satisfied customers are saying about our services.
                 </p>
                 </div>
                 <div className="rater-content">
+                    <div className="rater-star">
+                        <img src={starIcon}></img>
+                        <img src={starIcon}></img>
+                        <img src={starIcon}></img>
+                        <img src={starIcon}></img>
+                        <img src={starIcon}></img>
+                    </div>
+                    <p>Rated 5 star in Reviews</p>
+                </div>
+                <div className="rater-content">
+                    <div className="rater-star">
+                        <img src={starIcon}></img>
+                        <img src={starIcon}></img>
+                        <img src={starIcon}></img>
+                        <img src={starIcon}></img>
+                        <img src={starIcon}></img>
+                    </div>
+                    <p>Rated 5 star in Report Guru</p>
+                </div>
+                <div className="rater-content">
+                    <div className="rater-star">
+                        <img src={starIcon}></img>
+                        <img src={starIcon}></img>
+                        <img src={starIcon}></img>
+                        <img src={starIcon}></img>
+                        <img src={starIcon}></img>
+                    </div>
+                    <p>Rated 5 star in BestTech</p>
+                </div>
+                
+                <div className="users-box">
+                    <div className="user-info">
+                        <img src={avatar1} alt="Profile Picture"/>
+                        <p>Colton Smith</p>
+                        <p>Verified Buyer</p>
+                    </div>
+                    <div className="user-subject">
+                        <p>"We needed the same printed design as the one we had ordered a week prior. Not only
+                            did they find the original order,but we also received it in time. Excellent!"
+                        </p>
+                    </div>
 
                 </div>
                 <div className="users-box">
+                    <div className="user-info">
+                        <img src={avatar3} alt="Profile Picture"/>
+                        <p>Irene Roberts</p>
+                        <p>Verified Buyer</p>
+                    </div>
+                    <div className="user-subject">
+                        <p>"Customer service is always excellent and very quick turn around. Completely delighted
+                            with the simplicity of the purchase and the speed of delivery"
+                        </p>
+                    </div>
 
+                </div>
+                <div className="users-box">
+                    <div className="user-info">
+                        <img src={avatar2} alt="Profile Picture"/>
+                        <p>Anne Wallace</p>
+                        <p>Verified Buyer</p>
+                    </div>
+                    <div className="user-subject">
+                        <p>"Put an order with this company and can only praise them for the very high standart. 
+                            Will definitely use them again and recommend them to everyone!"
+                        </p>
+                    </div>
 
                 </div>
             </div>
-        </div>
+            </main>
     )
 }
 
